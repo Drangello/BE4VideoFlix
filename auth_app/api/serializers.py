1e4726fa253522498f329a358343ed8f92cdd342
Florian Weimann <drangello0505@gmail.com>
2026-06-29 12:44:59 +0200

    Add RegisterSerializer for user registration with email validation

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,1 +1,57 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
 
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """Serializer for user registration."""
+
+    confirmed_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "password",
+            "confirmed_password",
+        )
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def validate_email(self, value):
+        """Validate email uniqueness."""
+
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                "Bitte überprüfe deine Eingaben und versuche es erneut."
+            )
+
+        return value
+
+    def validate(self, attrs):
+        """Validate matching passwords."""
+
+        password = attrs.get("password")
+        confirmed = attrs.get("confirmed_password")
+
+        if password != confirmed:
+            raise serializers.ValidationError(
+                "Bitte überprüfe deine Eingaben und versuche es erneut."
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        """Create inactive user."""
+
+        validated_data.pop("confirmed_password")
+
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+            is_active=False,
+        )
+
+        return user
