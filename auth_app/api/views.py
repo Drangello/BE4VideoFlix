be1024dc54e230687bede35629f75c1ec4ffa8ed
Florian Weimann <drangello0505@gmail.com>
2026-06-29 13:31:25 +0200

    Refactor RegisterView and token_service: integrate activation token generation and streamline response handling

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,9 +1,9 @@
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from auth_app.api.serializers import RegisterSerializer
+from auth_app.services.token_service import create_activation_data
 from auth_app.services.user_service import create_inactive_user
+from common.responses import created_response
 
 
 class RegisterView(APIView):
@@ -18,13 +18,14 @@ class RegisterView(APIView):
             password=serializer.validated_data["password"],
         )
 
-        return Response(
+        activation_data = create_activation_data(user)
+
+        return created_response(
             {
                 "user": {
                     "id": user.id,
                     "email": user.email,
                 },
-                "token": "",
-            },
-            status=status.HTTP_201_CREATED,
+                "token": activation_data["token"],
+            }
         )
--- auth_app/services/token_service.py
+++ auth_app/services/token_service.py
@@ -1,1 +1,24 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
+
+def create_activation_data(user):
+    """Create uid and token for account activation."""
+
+    return {
+        "uidb64": urlsafe_base64_encode(force_bytes(user.pk)),
+        "token": default_token_generator.make_token(user),
+    }
+
+
+def get_user_id_from_uid(uidb64):
+    """Decode uidb64 and return user id."""
+
+    return force_str(urlsafe_base64_decode(uidb64))
+
+
+def is_valid_token(user, token):
+    """Return whether the given token is valid for the user."""
+
+    return default_token_generator.check_token(user, token)
