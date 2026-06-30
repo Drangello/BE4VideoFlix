bbd9d7d21ed3139c0536bf24afa06297ed920fb0
Florian Weimann <drangello0505@gmail.com>
2026-06-30 09:55:41 +0200

    Add build_activation_url function to construct activation URLs for frontend

--- auth_app/services/email_service.py
+++ auth_app/services/email_service.py
@@ -1,1 +1,14 @@
+from urllib.parse import urljoin
 
+from django.conf import settings
+
+
+def build_activation_url(uidb64, token):
+    """Build the frontend activation URL."""
+
+    activation_path = f"/activate/{uidb64}/{token}/"
+
+    return urljoin(
+        settings.FRONTEND_BASE_URL,
+        activation_path,
+    )
