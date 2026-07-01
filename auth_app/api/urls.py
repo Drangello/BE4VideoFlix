e72fbdd5cb34685c7ab9719c2d876ce95ebb7cda
Florian Weimann <drangello0505@gmail.com>
2026-07-01 10:09:54 +0200

    Refactor URL patterns to include PasswordResetView and adjust LogoutView placement

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -3,19 +3,25 @@ from django.urls import path
 from auth_app.api.views import (
     ActivateView,
     LoginView,
+    LogoutView,
+    PasswordResetView,
     RegisterView,
     TokenRefreshView,
-    LogoutView,
 )
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path(
+        "password_reset/",
+        PasswordResetView.as_view(),
+        name="password-reset",
+    ),
     path(
         "activate/<uidb64>/<token>/",
         ActivateView.as_view(),
         name="activate",
     ),
-    path("logout/", LogoutView.as_view(), name="logout"),
 ]
