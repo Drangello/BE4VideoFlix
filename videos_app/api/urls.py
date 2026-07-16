1c3c1182f65a0de1b37201f03364b4428a1690a8
Florian Weimann <drangello0505@gmail.com>
2026-07-16 11:29:08 +0200

    Add HLS playlist and segment URL patterns to API

--- videos_app/api/urls.py
+++ videos_app/api/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 
-from videos_app.api.views import VideoListView
+from videos_app.api.views import (
+    HLSPlaylistView,
+    HLSSegmentView,
+    VideoListView,
+)
 
 urlpatterns = [
     path("video/", VideoListView.as_view(), name="video-list"),
+    path(
+        "video/<int:movie_id>/<str:resolution>/index.m3u8",
+        HLSPlaylistView.as_view(),
+        name="hls-playlist",
+    ),
+    path(
+        "video/<int:movie_id>/<str:resolution>/<str:segment>/",
+        HLSSegmentView.as_view(),
+        name="hls-segment",
+    ),
 ]
