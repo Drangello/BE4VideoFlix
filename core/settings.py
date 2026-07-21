c27f19df472dbb2f279b068711334cd4bdd8c980
Florian Weimann <drangello0505@gmail.com>
2026-07-21 13:30:10 +0200

    Update email configuration: set default EMAIL_BACKEND and DEFAULT_FROM_EMAIL in .env.template

--- core/settings.py
+++ core/settings.py
@@ -178,7 +178,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.smtp.EmailBackend",
+)
 EMAIL_HOST = os.getenv("EMAIL_HOST", "")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
